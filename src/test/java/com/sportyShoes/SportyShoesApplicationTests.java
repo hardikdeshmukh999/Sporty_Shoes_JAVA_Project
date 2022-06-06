fba68223d0f09572fb78e54d636a23628f664004
Hardik Deshmukh <smarthardik10@gmail.com>
2022-06-06 12:41:50 +0530

    Add files via upload

---
+++ src/test/java/com/sportyShoes/SportyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyShoes;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
